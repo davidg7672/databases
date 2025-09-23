2e3328ce4fdeb2f30a090d80c4ecb2e391199c68
DavidSosa <davidgsosa1@gmail.com>
2025-09-22 17:53:19 -0700

    Made fixed, need to relook at documentation

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # CPSC 321 - Database Management Systems
 
-- Code, Homework, & Notes for CPSC 321
+-   Code, Homework, & Notes for CPSC 321
+
+## Homework
+
+##### Homework 1
+
+-   Reading
+-   Schema Diagrams (Primary Keys, Foreign Keys, etcs)
+
+##### Homework 2
+
+-   Airline SQL Implementation
+-   Spotify SQL Implementation & Schema Diagram
+    -   Junction Tables, Constraints, etc
+
+##### Homework 3
+
+## Author
+
+-   David Sosa - dsosavidal@zagmail.gonzaga.edu
--- homework/hw-2/hw2_3.sql
+++ homework/hw-2/hw2_3.sql
@@ -7,18 +7,7 @@
  * 
  *======================================================================*/
 
-
--- TODO:
---   * Fill in your name above and a brief description.
---   * Implement the question 1 schema as per the homework instructions.
---   * Populate each table according to the homework instructions.
---   * Be sure each table has a comment describing its purpose.
---   * Be sure to add comments as needed for attributes.
---   * Be sure your SQL code is well formatted (according to the style guides).
---   * Add two INSERT statements per table that violate constraints and
---     comment these out for the final submission
-
--- dropping tables
+-- Dropping Tables
 DROP TABLE IF EXISTS album_track;
 DROP TABLE IF EXISTS musician_track;
 DROP TABLE IF EXISTS track;
@@ -31,32 +20,55 @@ DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS album;
 DROP TABLE IF EXISTS music_group;
 
+-- ======================================================================
+-- Table: music_group
+-- Purpose: Represents a musical group (band,artist collective).
+-- ======================================================================
 CREATE TABLE music_group (
     name VARCHAR(40) NOT NULL,
-    yr_formed SMALLINT NOT NULL,
+    yr_formed SMALLINT NOT NULL CHECK (yr_formed >= 1900),
 
     PRIMARY KEY (name)
 );
+-- Violating INSERTs:
+-- INSERT INTO music_group VALUES ('', 1990);              -- empty name not allowed
+-- INSERT INTO music_group VALUES ('The Beatles', 1800);   -- too early
 
+-- ======================================================================
+-- table: album
+-- Purpose: Represents an album created by a music group
+-- ======================================================================
 CREATE TABLE album (
     title VARCHAR(40) NOT NULL,
-    yr_recorded SMALLINT NOT NULL,
+    yr_recorded SMALLINT NOT NULL CHECK (yr_recorded >= 1900),
     group_name VARCHAR(40) NOT NULL,
     record_label VARCHAR(40) NOT NULL,
 
     PRIMARY KEY (title, group_name),
     FOREIGN KEY (group_name) REFERENCES music_group(name)
 );
+-- Violating INSERTs:
+-- INSERT INTO album VALUES ('Abbey Road', 1890, 'The Beatles', 'EMI'); -- bad year
+-- INSERT INTO album VALUES ('Nevermind', 1991, 'NonExistentBand', 'DGC'); -- FK violation
 
--- create junction table
-
+-- ======================================================================
+-- Table: genre
+-- Purpose: Represents a musical genre
+-- ======================================================================
 CREATE TABLE genre (
     name VARCHAR(40) NOT NULL,
     description TEXT NOT NULL,
 
     PRIMARY KEY (name)
 );
+-- Violating INSERTs:
+-- INSERT INTO genre VALUES (NULL, 'Genre with null name'); -- null name
+-- INSERT INTO genre VALUES ('Rock', NULL); -- null description
 
+-- ======================================================================
+-- Table: group_genre
+-- Purpose: Junction table mapping groups to genre
+-- ======================================================================
 CREATE TABLE group_genre (
     group_name VARCHAR(40) NOT NULL,
     genre_name VARCHAR(40) NOT NULL,
@@ -65,56 +77,94 @@ CREATE TABLE group_genre (
     FOREIGN KEY (group_name) REFERENCES music_group(name),
     FOREIGN KEY (genre_name) REFERENCES genre(name)
 );
+-- Violating INSERTs:
+-- INSERT INTO group_genre VALUES ('n/a band', 'Rock'); -- fk violation
+-- INSERT INTO group_genre VALUES ('The Beatles', 'n/a genre'); -- fk violation
 
+-- ======================================================================
+-- Table: musician
+-- Purpose: Purpose: Represent an individual musician
+-- ======================================================================
 CREATE TABLE musician (
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
     stage_name VARCHAR(40),
-    yr_born SMALLINT,
+    yr_born SMALLINT CHECK (yr_born >= 1900),
 
-    PRIMARY KEY (first_name, last_name)
+    PRIMARY KEY (first_name, last_name),
 );
+-- Violating INSERTs:
+-- INSERT INTO musician VALUES ('Ringo', 'Starr', 'Ringo', 1899); -- too early
+-- INSERT INTO musician VALUES (NULL, 'Lennon', 'JD', 1950); -- null first name
 
+-- ======================================================================
+-- Table: musician_group
+-- Purpose: Junction table that maps musicians to groups they played in
+-- ======================================================================
 CREATE TABLE musician_group (
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
     group_name VARCHAR(40) NOT NULL,
     start_yr SMALLINT NOT NULL,
-    end_yr SMALLINT,
+    end_yr SMALLINT CHECK (end_yr IS NULL OR end_yr >= start_yr),
 
     PRIMARY KEY (first_name, last_name, group_name),
     FOREIGN KEY (first_name, last_name) REFERENCES musician(first_name, last_name),
     FOREIGN KEY (group_name) REFERENCES music_group(name)
 );
+-- Violating INSERTs:
+-- INSERT INTO musician_group VALUES ('John', 'Lennon', 'The Beatles', 1800, 1970); -- bad year
+-- INSERT INTO musician_group VALUES ('John', 'Lennon', 'The Beatles', 1960, 1949); -- year before
 
--- influencing
+-- ======================================================================
+-- Table: influence
+-- Purpose: Captures influence relationships between groups (e.g. Arctic Monkeys Influenced by The Strokes)
+-- ======================================================================
 CREATE TABLE influence (
-    influencer VARCHAR(40),
-    influenced VARCHAR(40),
+    influencer VARCHAR(40) NOT NULL,
+    influenced VARCHAR(40) NOT NULL,
 
     PRIMARY KEY (influencer, influenced),
     FOREIGN KEY (influencer) REFERENCES music_group(name),
     FOREIGN KEY (influenced) REFERENCES music_group(name)
+    CONSTRAINT no_self_influence CHECK (influencer <> influenced) -- making sure the influencer isn't themselves
 );
+-- INSERT INTO influence VALUES ('The Beatles', 'The Beatles') -- influencer influencing himself, can't be true
+-- INSERT INTO influence VALUES ('The Beatles', NULL) -- can't be null
 
 -- tracks & songs
+-- ======================================================================
+-- Table: song
+-- Purpose: Represents a song and when it was written
+-- ======================================================================
 CREATE TABLE song (
     title VARCHAR(40) UNIQUE NOT NULL,
-    yr_written SMALLINT NOT NULL,
+    yr_written SMALLINT NOT NULL CHECK (yr_written >= 1900),
 
     PRIMARY KEY (title)
 );
+-- INSERT INTO song VALUES ('Don't Let Me Down', 1899) -- invalid year
+-- INSERT INTO song VALUES (NULL, 1899) -- can't be null
 
+-- ======================================================================
+-- Table: track
+-- Purpose: Represents a track, when it was made, and it's unique identified
+-- ======================================================================
 CREATE TABLE track (
     id INT NOT NULL,
     song_title VARCHAR(40) NOT NULL,
-    yr_recorded SMALLINT NOT NULL,
+    yr_recorded SMALLINT NOT NULL CHECK (yr_recorded >= 1900),
 
     PRIMARY KEY (id),
     FOREIGN KEY (song_title) REFERENCES song(title)
 );
+-- INSERT INTO track VALUES (1, 'Don't Let Me Down', 1899) -- year not valid
+-- INSERT INTO track VALUES (NULL, 'Don't Let Me Down', 1970) -- can't be null
 
--- musicians that contributed to a track
+-- ======================================================================
+-- flight(airline, flight_number, departure, arrival, flights_per_wk)
+-- Stores information about flights.
+-- ======================================================================
 CREATE TABLE musician_track (
     track_id INT NOT NULL,
     musician_first_name VARCHAR(40) NOT NULL,
@@ -125,7 +175,31 @@ CREATE TABLE musician_track (
     FOREIGN KEY (musician_first_name, musician_last_name) REFERENCES musician(first_name, last_name)
 );
 
+--- 
+-- INSERT INTO musician_track VALUES (1, 'Don't Let Me Down', 1899) -- year not valid
+-- INSERT INTO musician_track VALUES (NULL, 'Don't Let Me Down', 1970) -- can't be null
+
+-- musicians that wrote a song
+-- very similar to musician_track
+-- ======================================================================
+-- flight(airline, flight_number, departure, arrival, flights_per_wk)
+-- Stores information about flights.
+-- ======================================================================
+CREATE TABLE musician_song (
+    song_title VARCHAR(40) NOT NULL,
+    musician_first_name VARCHAR(40) NOT NULL,
+    musician_last_name VARCHAR(40) NOT NULL,
+
+    PRIMARY KEY (song_title, musician_first_name, musician_last_name),
+    FOREIGN KEY (song_title) REFERENCES song(title),
+    FOREIGN KEY (musician_first_name, musician_last_name) REFERENCES musician(first_name, last_name)
+);
+
 -- tracks can appear on different albums
+-- ======================================================================
+-- flight(airline, flight_number, departure, arrival, flights_per_wk)
+-- Stores information about flights.
+-- ======================================================================
 CREATE TABLE album_track (
     album_title VARCHAR(40) NOT NULL,
     group_name VARCHAR(40) NOT NULL,
